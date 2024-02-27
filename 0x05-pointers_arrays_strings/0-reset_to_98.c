a9c370fc05889ce95f2907a9bf3ec3feecf4961f
nwachukwu ikedinachi <bldrnwagod@gmail.com>
2024-02-27 08:01:45 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * reset_to_98 - A function that takes a pointer to an integer
+ * and changes it to 98
+ * @n: input parameter
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{	
+	*n = 98;
+}
